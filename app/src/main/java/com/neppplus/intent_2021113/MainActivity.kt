51b0b4c10b2d437d7bc8028452044ef4a53b141c
yeonjin seong <syj3405@naver.com>
2021-11-13 15:06:44 +0900

    Intent이용 - 화면 전환

--- app/src/main/java/com/neppplus/intent_2021113/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_2021113/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.neppplus.intent_2021113
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnMoveToOther.setOnClickListener {
+
+//              OtherActivity 화면으로 이동하기.
+
+            val myIntent = Intent( this , OtherActivity::class.java )
+            startActivity(myIntent)
+
+
+        }
     }
 }
