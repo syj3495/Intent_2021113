20cce4b265e58900b221cc966478e35bbe9ae0af
yeonjin seong <syj3405@naver.com>
2021-11-20 10:38:30 +0900

    DIAL액션 체험 UI작업

--- app/src/main/java/com/neppplus/intent_2021113/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_2021113/MainActivity.kt
@@ -16,6 +16,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnDial.setOnClickListener {
+
+//            입력한 전화번호? 추출 ( 변수에 저장 )
+
+//            그 전화번호에 실제 전화 연결 (DAIL)
+
+
+
+        }
+
 
         btnMoveToOther.setOnClickListener {
 
@@ -61,5 +71,33 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        requestCode : 어떤 것을 가지러 다녀온건지 알려주는 숫자가 기록됨
+
+        if ( requestCode == REQ_FOR_NICKNAME) {
+
+//            닉네임을 가지러 다녀왔을 때 실행되는 중괄호
+
+//            resultCode : 확인 (OK) / 취소 (CANCEL) 중 어떤것을 눌렀는 지 알려줌.
+
+            if (resultCode == RESULT_OK ) {
+
+//                닉네임을 가지러가서 -> 확인도 누른게 맞을 때 실행되는 코드.
+
+//                data : 이전 화면에서 담아준 resultIntent를 들고 있는 역할.
+
+                val newNickname = data?.getStringExtra("nick")
+
+                txtNickname.text = newNickname
+
+
+            }
+
+        }
+
     }
 }
